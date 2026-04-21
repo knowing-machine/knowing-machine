447f3a2b656e08dc51d8c4085bcb5923406298d9
Alex Fedotov <alxfed@gmail.com>
2026-04-21 10:03:26 -0500

    initial release

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -14,14 +14,14 @@ def test_uvx_integration():
     
     input_text = "Theodotos-Alexandreus: Are language models seeking the Truth, machine?\n"
     
-    # "uvx --from . name-of-the-machine" behaves identically to PyPI "uvx name-of-the-machine"
+    # "uvx --from . knowing-machine" behaves identically to PyPI "uvx knowing-machine"
     cmd = [
         "uvx", 
         "--with",
         ".[all]",
         "--from", 
         ".", 
-        "name-of-the-machine"
+        "knowing-machine"
     ]
     
     # The keys/tokens are passed through the environment automatically via env=env.
